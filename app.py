79a4657b5d915d36df017fd7028a998ca2901465
Dishav2 <105136321+Dishav2@users.noreply.github.com>
2023-08-18 22:22:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -118,7 +118,6 @@ with gr.Blocks() as demo:
     interface.chatbot.value = get_first_message([])
 
 # demo.queue().launch(server_port=7861)
-#port = int(os.environ.get('PORT', 7861))
-#demo.queue().launch(share=True, server_port=port)
-demo.queue().launch(share=True,)
+port = int(os.environ.get('PORT', 7861))
+demo.queue().launch(server_name="0.0.0.0", server_port=port)
    
